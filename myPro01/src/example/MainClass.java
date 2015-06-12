efa7aa1cc7a4c7169c5564c7390042c676756f2f
Taylor <xognsl99@naver.com>
2015-06-12 09:47:21 +0900

    myPro_TaeHoon612A_ver01

---
+++ myPro01/src/example/MainClass.java
@@ -0,0 +1,11 @@
+package example;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a = new A();
+		System.out.println("안녕하세요 미스코리아 좌민혜입니다.");
+	}
+
+}

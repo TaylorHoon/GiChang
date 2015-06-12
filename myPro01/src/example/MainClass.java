d2f4b60e571ace2fbd5a2cddbcd218e7c9cc2629
Taylor <Taylor@Taylor-PC>
2015-06-12 10:41:44 +0900

    myPro_TaeHoon612A_ver03

--- myPro01/src/example/MainClass.java
+++ myPro01/src/example/MainClass.java
@@ -6,6 +6,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		A a = new A();
 		B b = new B();
+		C c = new C();
 		System.out.println("안녕하세요 미스코리아 좌민혜입니다.");
 		System.out.println("누구냐 넌");
 	}
